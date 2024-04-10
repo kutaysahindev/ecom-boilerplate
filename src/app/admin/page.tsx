e9846d20cb3937a452f7029179a18b6adec13b64
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-04-10 21:38:30 +0300

    Add product card & Fetch product data from the db

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -37,10 +37,20 @@ async function getUserData() {
   };
 }
 
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+
+  return { activeCount, inactiveCount };
+}
+
 export default async function AdminDashboard() {
-  const [salesData, userData] = await Promise.all([
+  const [salesData, userData, productData] = await Promise.all([
     getSalesData(),
     getUserData(),
+    getProductData(),
   ]);
 
   return (
@@ -56,6 +66,11 @@ export default async function AdminDashboard() {
           userData.averageValuePerUser
         )} Average Value`}
         body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title="Active Products"
+        subtitle={`${formatNumber(productData.inactiveCount)} Inactive`}
+        body={formatNumber(productData.activeCount)}
       />
     </div>
   );
