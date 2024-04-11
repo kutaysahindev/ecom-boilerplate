2db34f6774691abde82b77ce535dea17780b3ba6
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-04-12 00:42:36 +0300

    Add admin products page

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "../_components/pageHeader";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+      </div>
+    </>
+  );
+}
