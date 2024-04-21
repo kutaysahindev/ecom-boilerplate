37339b1a3a6e549c1664584f454d8a6b2b095850
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-04-21 18:56:11 +0300

    Add "Add Product" button

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/pageHeader";
+import Link from "next/link";
 
 export default function AdminProductsPage() {
   return (
     <>
       <div className="flex justify-between items-center gap-4">
         <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
       </div>
     </>
   );
