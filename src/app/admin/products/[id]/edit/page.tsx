2100146481a1447011cf30fc976d2f349094385a
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-05-01 01:35:42 +0300

    Add new routes

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "../../../_components/pageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+
+export default function EditProductPage() {
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm />
+    </>
+  );
+}
