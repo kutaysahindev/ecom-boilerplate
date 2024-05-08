788121232073fedb62fab98be6eefdd742a487a7
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-05-08 16:41:48 +0300

    Add title section to page.tsx

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/button";
 import db from "@/db/db";
+import { Product } from "@prisma/client";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 function getMostPopularProducts() {
   return db.product.findMany({
@@ -16,5 +20,37 @@ function getNewestProducts() {
 }
 
 export default function HomePage() {
-  return <main></main>;
+  return (
+    <main className="space-y-12">
+      <ProductGridSection
+        title="Most Popular"
+        productsFetcher={getMostPopularProducts}
+      />
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+    </main>
+  );
+}
+
+type ProductGridSectionProps = {
+  title: string;
+  productsFetcher: () => Promise<Product[]>;
+};
+
+function ProductGridSection({
+  productsFetcher,
+  title,
+}: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button asChild variant="outline" className="space-x-2">
+          <Link href="/products">
+            <span>View All</span>
+            <ArrowRight className="size-4"></ArrowRight>
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
