81175f3ed4fad54c83299825451a89ecac057a9a
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-05-08 20:06:26 +0300

    Add ProductCard.tsx to main page

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,3 +1,4 @@
+import { ProductCard } from "@/components/ProductCard";
 import { Button } from "@/components/ui/button";
 import db from "@/db/db";
 import { Product } from "@prisma/client";
@@ -36,7 +37,7 @@ type ProductGridSectionProps = {
   productsFetcher: () => Promise<Product[]>;
 };
 
-function ProductGridSection({
+async function ProductGridSection({
   productsFetcher,
   title,
 }: ProductGridSectionProps) {
@@ -50,6 +51,11 @@ function ProductGridSection({
             <ArrowRight className="size-4"></ArrowRight>
           </Link>
         </Button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {(await productsFetcher()).map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
       </div>
     </div>
   );
